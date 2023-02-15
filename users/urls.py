a73505ad4190f2bb6a3511481db86eb51d205cff
jeangho293 <jeangho293@naver.com>
2023-02-15 21:26:12 +0900

    간단한 게시글 어플리케이션을 만들기 위해 리셋

--- users/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
-from users import views
-
-urlpatterns = [path("users/", views.UserView.as_view())]
-urlpatterns = format_suffix_patterns(urlpatterns)
